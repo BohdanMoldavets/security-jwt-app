59625ea303bdc15a3fb47b63927d662ea6366d40
Bohdan <143205638+BohdanMoldavets@users.noreply.github.com>
2025-03-07 17:28:36 +0100

    add migration db file

---
+++ src/main/resources/db/migration/init.sql
@@ -0,0 +1,35 @@
+CREATE TABLE users {
+    id SERIAL,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(80) NOT NULL,
+    email VARCHAR(50) UNIQUE,
+    PRIMARY KEY (id)
+}
+
+CREATE TABLE roles {
+    id SERIAL,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+}
+
+INSERT INTO roles (name)
+VALUES
+('ROLE_ADMIN'), ('ROLE_USER');
+
+INSERT INTO users(username, password, email)
+VALUES
+('user', '', 'user@gmail.com'), -- todo crypt password
+('admin', '', 'admin@gmail.com');
+
+CREATE TABLE users_roles {
+    user_id INT NOT NULL,
+    role_id INT NOT NULL,
+    PRIMARY KEY (user_id, role_id),
+    FOREIGN KEY (user_id) REFERECES user (id),
+    FOREIGN KEY (role_id) REFERECES user (id)
+}
+
+INSERT INTO users_roles(user_id,role_id)
+VALUES
+(1,1),
+(2,2);
